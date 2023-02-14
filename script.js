574f9c7f245daeda15a6677b599f57b60fab7bd3
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-02-14 11:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+document.querySelector('#addTodoBtn').onclick = function(){
+    if(document.querySelector('#newTodoInput').value.length == 0){
+        alert("Kindly Enter Task Name!!!!")
+    }
+
+    else{
+        document.querySelector('#todoList').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newTodoInput').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+    }
+}
